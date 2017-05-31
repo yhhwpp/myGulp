5050c571305aa6c45e32bf552c3d9053378b9c7f
yuan hehe <helpzp2008@126.com>
2017-05-31 12:14:27 +0800

    增加ignore file

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,48 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     clean = require('gulp-clean'),
-    imagemin = require('gulp-imagemin');
-browserSync = require('browser-sync').create(),
+    imagemin = require('gulp-imagemin'),
+    browserSync = require('browser-sync').create(),
+    rev = require('gulp-rev'),
+    revCollector = require('gulp-rev-collector'),
     reload = browserSync.reload,
     minifyCSS = require('gulp-minify-css'),
+    htmlminify = require('gulp-html-minify'),
     copy = require("gulp-copy");
 
 //压缩js
 gulp.task('minifyjs', function (cb) {
     return gulp.src('js/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+        .pipe(rev())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('dist/rev/js'));
 });
 
 //压缩css
 gulp.task('minifyCSS', function (cb) {
     return gulp.src('css/*.css')
         .pipe(minifyCSS())
+        .pipe(rev())
         .pipe(gulp.dest('dist/css'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('dist/rev/css'));
 });
 
 // clean
 gulp.task('clean', function () {
-    return gulp.src('build')
+    return gulp.src(['build', 'dist'])
         .pipe(clean())
+});
+
+gulp.task('rev-html', ['minifyjs', 'minifyCSS'], function () {
+    return gulp.src(['dist/rev/**/*.json', './index.html'])
+        .pipe(revCollector({
+            replaceReved: true
+        }))
+        // .pipe(htmlminify())
+        .pipe(gulp.dest('build/'))
 });
 //合并 
 gulp.task('concat', ['minifyjs', 'minifyCSS'], function () {
